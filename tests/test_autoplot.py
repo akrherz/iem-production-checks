5b220456c17ae4f9f838be35e86a9852e810235c
akrherz <akrherz@iastate.edu>
2019-10-24 12:48:45 -0500

    add testing for autoplots

---
+++ tests/test_autoplot.py
@@ -0,0 +1,63 @@
+"""Attempt to call each autoplot and whine about API calls that error"""
+
+import pytest
+import requests
+
+SERVICE = "https://mesonet.agron.iastate.edu"
+
+
+def get_formats(i):
+    """Figure out which formats this script supports"""
+    uri = "%s/plotting/auto/meta/%s.json" % (SERVICE, i)
+    try:
+        res = requests.get(uri, timeout=10)
+    except requests.exceptions.ReadTimeout:
+        print("%s. %s -> Read Timeout" % (i, uri[16:]))
+    if res.status_code == 404:
+        print("scanning metadata got 404 at i=%s, proceeding" % (i, ))
+        return False
+    if res.status_code != 200:
+        print("%s. %s -> HTTP: %s" % (i, uri, res.status_code))
+        print(res.text)
+    try:
+        json = res.json()
+    except Exception as exp:
+        print("%s %s -> json failed\n%s" % (i, res.content, exp))
+        return
+    fmts = ['png', ]
+    if 'report' in json and json['report']:
+        fmts.append('txt')
+    if 'highcharts' in json and json['highcharts']:
+        fmts.append('js')
+    if 'mapbox' in json and json['mapbox']:
+        fmts.append('mapbox')
+    if json.get('data', False):
+        fmts.append('csv')
+        fmts.append('xlsx')
+    return fmts
+
+
+def get_all():
+    """Figure out what we need to run for."""
+    url = "%s/plotting/auto/meta/0.json" % (SERVICE, )
+    j = requests.get(url).json()
+    queue = []
+    for lbl in j['plots']:
+        for opt in lbl['options']:
+            queue.append(opt['id'])
+    return queue
+
+
+@pytest.mark.parametrize("opts", get_all())
+def test_autoplot(opts):
+    """Run this plot"""
+    i = opts
+    for fmt in get_formats(i):
+        uri = "%s/plotting/auto/plot/%s/dpi:100::_cb:1.%s" % (SERVICE, i, fmt)
+        res = requests.get(uri, timeout=600)
+        print(
+            "i: %s fmt: %s status_code: %s len(response): %s" % (
+                i, fmt, res.status_code, len(res.content)))
+        # Known failures likely due to missing data
+        assert res.status_code in [200, 400]
+        assert res.content != ""
