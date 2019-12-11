faf070006b1eaba3e24a1fab92bc9f1c8cc17b04
daryl herzmann <akrherz@iastate.edu>
2019-12-10 23:14:45 -0600

    test IEM /json/ example URLs (#2)
    * test IEM /json/ example URLs
    
    * add lxml

--- tests/test_iem.py
+++ tests/test_iem.py
@@ -1,6 +1,28 @@
 """Run Tests."""
 
 import requests
+import pytest
+from bs4 import BeautifulSoup
+
+
+def get_jsonlinks():
+    """Figure out what we need to run for."""
+    content = requests.get("https://mesonet.agron.iastate.edu/json/").content
+    soup = BeautifulSoup(content, 'lxml')
+    queue = []
+    for tag in soup.find_all('a'):
+        if tag.text != 'Example JSON':
+            continue
+        queue.append(tag.attrs['href'])
+    return queue
+
+
+@pytest.mark.parametrize("opts", get_jsonlinks())
+def test_json_documentation_page_links(opts):
+    """Test example URLs shown on the /json/ page."""
+    res = requests.get(opts, timeout=60)
+    assert res.status_code == 200
+    assert res.json()
 
 
 def test_urls():
